e20ae86bd201e40b530e25ddddaf6c439f6f430a
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-01-22 00:14:02 +0200

    Change MaximumDegree example specification

--- src/main/java/gr/james/simplegraph/examples/MaximumDegree.java
+++ src/main/java/gr/james/simplegraph/examples/MaximumDegree.java
@@ -3,13 +3,14 @@ package gr.james.simplegraph.examples;
 import gr.james.simplegraph.Graph;
 
 /**
- * Get the vertex with the maximum degree from a {@link Graph}.
+ * Get the maximum degree in a {@link Graph}.
  */
 public class MaximumDegree {
     /**
      * Get the maximum degree in a {@link Graph}.
      * <p>
-     * Returns {@code 0} if the graph has no edges.
+     * Returns {@code -1} if the graph has no vertices. Returns {@code 0} if the graph has at least one vertex but no
+     * edges. The degree of a vertex includes a self-loop, if present.
      * <p>
      * Complexity: O(V)
      *
@@ -18,7 +19,7 @@ public class MaximumDegree {
      * @throws NullPointerException if {@code g} is {@code null}
      */
     public static int maximumDegree(Graph g) {
-        int max = 0;
+        int max = -1;
         for (int i = 0; i < g.size(); i++) {
             final int degree = g.getEdges(i).size();
             if (degree > max) {
