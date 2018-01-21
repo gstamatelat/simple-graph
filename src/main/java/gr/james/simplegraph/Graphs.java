e0879007b22d72af3a2f3bf81c9be1508c902a3a
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-01-21 16:09:25 +0200

    Implement Graphs.isWeightLegal

--- src/main/java/gr/james/simplegraph/Graphs.java
+++ src/main/java/gr/james/simplegraph/Graphs.java
@@ -21,9 +21,28 @@ final class Graphs {
      * @throws IllegalArgumentException if {@code weight} is not legal
      */
     static double checkWeight(double weight) {
-        if (Double.isNaN(weight) || Double.isInfinite(weight)) {
+        if (!isWeightLegal(weight)) {
             throw new IllegalArgumentException();
         }
         return weight;
+    }
+
+    /**
+     * Check is a weight value is legal.
+     * <p>
+     * A weight value is legal if it is not any of the following values:
+     * <ul>
+     * <li>{@link Double#NaN}</li>
+     * <li>{@link Double#POSITIVE_INFINITY}</li>
+     * <li>{@link Double#NEGATIVE_INFINITY}</li>
+     * </ul>
+     * <p>
+     * Complexity: O(1)
+     *
+     * @param weight the weight value to check
+     * @return {@code true} if {@code weight} is legal, otherwise {@code false}
+     */
+    static boolean isWeightLegal(double weight) {
+        return !Double.isNaN(weight) && !Double.isInfinite(weight);
     }
 }
