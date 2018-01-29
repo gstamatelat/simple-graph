948d398ab1099b3e8401185678cbdc2284fa31d0
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-01-29 14:21:14 +0200

    Correct package-info (#19)

--- src/main/java/gr/james/simplegraph/package-info.java
+++ src/main/java/gr/james/simplegraph/package-info.java
@@ -1,5 +1,5 @@
 /**
- * The mutable and immutable graph classes.
+ * The graph interfaces and the respective mutable and immutable implementations.
  * <h2>Vertices and edges</h2>
  * For simplicity, vertices are, for each graph instance, represented using integers in the range {@code [0,V)}. There
  * is no inherent edge object notion; all edges are anonymous but can have a weight assigned on weighted graph types.
@@ -12,20 +12,17 @@
  * the adjacent nodes of a vertex that doesn't exist or when an argument is {@code null}. These exceptions exist to
  * augment the robustness of a client program and should not be catched.
  * <h2>Mutable and immutable types</h2>
- * All immutable graph classes have their {@code Mutable*} counterparts.
+ * All interfaces have a {@code Mutable*} and {@code Immutable*} implementation.
  * <p>
  * Mutable graphs should mainly be treated as graph builders because they have methods that support mutation, like
  * vertex/edge addition/removal etc. Instances of immutable graphs can be created only using the method
  * {@code toImmutable()} of the respective mutable graph. For example
  * {@link gr.james.simplegraph.MutableGraph#toImmutable()} will create a copy of the graph and return it as a
- * {@link gr.james.simplegraph.ImmutableGraph}. Mutable graphs have various copy constructors from compatible types. For example,
- * {@link gr.james.simplegraph.MutableGraph#MutableGraph(WeightedGraph)} will construct a
+ * {@link gr.james.simplegraph.ImmutableGraph}. Mutable graphs have various copy constructors from compatible types. For
+ * example, {@link gr.james.simplegraph.MutableGraph#MutableGraph(WeightedGraph)} will construct a
  * {@link gr.james.simplegraph.MutableGraph} from a copy of the argument.
  * <p>
  * Immutable graphs do not have mutation methods and are, for example, appropriate to use as arguments to algorithms.
- * Immutable graphs guarantee thread safety and deterministic iteration and behavior. There exist constant time
- * decorators around compatible types of immutable graphs {@code asGraph()}, {@code asDirected()}, {@code asWeighted()},
- * {@code asWeightedDirected()}. For example, {@link gr.james.simplegraph.ImmutableGraph#asDirected()} will return a
- * {@link gr.james.simplegraph.ImmutableDirectedGraph} decorator of that graph.
+ * Immutable graphs guarantee thread safety and deterministic iteration and behavior.
  */
 package gr.james.simplegraph;
